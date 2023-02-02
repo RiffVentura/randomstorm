c352e08397178f15fef8e8e1acaa4f6f2cf3b32c
Riff Ventura <riff.ventura@gmail.com>
2023-02-02 21:48:35 +0100

    added light theme configuration

--- src/theme.ts
+++ src/theme.ts
@@ -1,3 +1,24 @@
 export const theme = {
-    color: { background: '#eeeeee' },
+    color: {
+        background: '#eeeeee',
+        cloud: '#bbeff1',
+        widgetBackground: '#f5f5f5',
+        locked: '#e9e9e9',
+        selected: '#6d6d6d',
+    },
+    typography: {
+        fontFamily: 'Montserrat, sans-serif',
+        weight: {
+            default: 'medium',
+            light: 'LightItalic',
+            bold: 'SemiBold',
+            icon: 'Bold',
+        },
+        size: {
+            default: 18,
+            title: 20,
+            big: 27,
+            icon: 32,
+        },
+    },
 };
